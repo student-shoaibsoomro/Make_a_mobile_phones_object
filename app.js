5b9f23de2128737247835db6a798c395bd96abb3
Hafiz Shoaib Siddiq <159219257+student-shoaibsoomro@users.noreply.github.com>
2024-03-19 10:27:55 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,24 @@
+let mobile = {
+    Name: "Samsung",
+    Model: "Galaxy",
+    Storage: '128GB',
+    Screen_Size: 6.7,
+    Color: "Edge White",
+    Battery: "5000mh",
+    Availability: true,
+};
+console.log(mobile);
+export {};
+// let person : person = {
+//     Name : 'Shoaib',
+//     age : 34,
+//     student : true,
+//     nationality : 'Pakistan',
+// }
+// interface person {
+//     Name : string,
+//     age : number,
+//     student : boolean,
+//     nationality : string,
+// }
+// console.log(person);
